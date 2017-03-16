b04f62f2e9b4380ef94114d085c4f129e2c2adf4
Mohammed Jasam <mnqnd@mst.edu>
2017-03-16 15:05:34 -0500

    Working with the Surprise Module

---
+++ Item.py
@@ -0,0 +1,70 @@
+from surprise import SVD
+from surprise import NMF
+from surprise import KNNBasic
+from surprise import Dataset
+from surprise import evaluate, print_perf
+from surprise import *
+from surprise import  Reader
+import os
+import numpy as np
+from six import iteritems
+from six import itervalues
+from itertools import product
+import csv
+import pprint
+
+# with open('a.csv', 'r') as f:  # the 'b' flag is required in some platforms
+#     reader = csv.reader(f)
+#     # pprint.pprint(list(reader))  # prints on console the data of the csv file
+#
+#     for row in reader:
+#         print(row)
+# from  import accuracy
+# from .dump import dump
+#load data from a file
+file_path = os.path.expanduser('restaurant_ratings.txt')
+reader = Reader(line_format='user item rating timestamp', sep='\t', skip_lines=99900)
+# print(reader)
+data = Dataset.load_from_file(file_path, reader=reader)
+data.folds()
+
+#Splitting data into 3 folds
+data.split(n_folds=3, shuffle=False)
+
+#Item based Collaborative Filtering algorithm.
+# algo = KNNBasic(sim_options = {'user_based': False})
+#Using the value of K
+# algo = KNNBasic(k=20, sim_options = {'name':'MSD', 'user_based': True })
+
+#To used MSD in Item Based Algorithm
+algo = KNNBasic(sim_options = {'name':'MSD','user_based': False })
+#To used Cosine in Item Based Algorithm
+#algo = KNNBasic(sim_options = {'name':'cosine','user_based': False })
+#To used Pearson in Item Based Algorithm
+#algo = KNNBasic(sim_options = {'name':'pearson','user_based': False })
+
+#Printing the result
+perf = evaluate(algo, data, measures=['RMSE', 'MAE'])
+
+def pt(performances):
+        # retrieve number of folds. Kind of ugly...
+    n_folds = [len(values) for values in itervalues(performances)][0]
+
+    row_format = '{:<8}' * (n_folds + 2)
+    s = row_format.format(
+        '',
+        *['Fold {0}'.format(i + 1) for i in range(n_folds)] + ['Mean'])
+    s += '\n'
+    s += '\n'.join(row_format.format(
+        key.upper(),
+        *['{:1.4f}'.format(v) for v in vals] +
+        ['{:1.4f}'.format(np.mean(vals))])
+        for (key, vals) in iteritems(performances))
+    print(s)
+
+pt(perf)
+
+with open('a.csv','w') as fo:
+    print_perf(perf,fo)
+
+#Visualization
---
+++ NMF.py
@@ -0,0 +1,31 @@
+from surprise import SVD
+from surprise import NMF
+from surprise import KNNBasic
+from surprise import Dataset
+from surprise import evaluate, print_perf
+from surprise import *
+from surprise import  Reader
+import os
+import numpy as np
+from six import iteritems
+from six import itervalues
+from itertools import product
+import csv
+import pprint
+#load data from a file
+file_path = os.path.expanduser('restaurant_ratings.txt')
+reader = Reader(line_format='user item rating timestamp', sep='\t', skip_lines=0)
+data = Dataset.load_from_file(file_path, reader=reader)
+data.folds()
+
+#Splitting data into 3 folds
+data.split(n_folds=3, shuffle=False)
+
+#NMF Algorithm
+algo = NMF()
+
+#Printing the result
+perf = evaluate(algo, data, measures=['RMSE', 'MAE'])
+print_perf(perf)
+
+#Visualization
